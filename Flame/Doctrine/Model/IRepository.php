ce267209ece2c23f25b7a23e2a66e6e7914c3287
JSifalda <sifalda.jiri@gmail.com>
2013-02-26 19:10:50 +0100

    Add source

---
+++ Flame/Doctrine/Model/IRepository.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * IRepository
+ *
+ * @author  Jiří Šifalda
+ * @package Flame
+ *
+ * @date    12.07.12
+ */
+
+namespace Flame\Doctrine\Model;
+
+interface IRepository
+{
+
+	const NO_FLUSH = true;
+
+	const FLUSH = false;
+}
