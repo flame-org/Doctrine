c78ba845b7da31866e74ff3aff5b9305febade0a
JSifalda <sifalda.jiri@gmail.com>
2013-06-30 17:51:33 +0200

    Fix annotations

--- Flame/Doctrine/EntityDao.php
+++ Flame/Doctrine/EntityDao.php
@@ -14,24 +14,22 @@ class EntityDao extends \Kdyby\Doctrine\EntityDao
 {
 
 	/**
-	 * @return \Sharezone\Doctrine\Entity
+	 * @return \Flame\Doctrine\Entity
 	 */
 	public function createEntity()
 	{
 		$reflection = new ClassType($this->getEntityName());
-
 		return $reflection->newInstanceArgs(func_get_args());
 	}
 
 	/**
-	 * @return \Sharezone\Doctrine\Entity
+	 * @return \Flame\Doctrine\Entity
 	 */
 	public function addNewEntity()
 	{
 		$reflection = new ClassType($this->getEntityName());
 		$entity = $reflection->newInstanceArgs(func_get_args());
 		$this->add($entity);
-
 		return $entity;
 	}
 
