5e6fd4ce2997a7cc07b269f59c70a1aae9844df7
JSifalda <sifalda.jiri@gmail.com>
2013-09-11 22:24:38 +0200

    Remove interface for validators, get validators from context

--- Flame/Doctrine/Values/DataSet.php
+++ Flame/Doctrine/Values/DataSet.php
@@ -8,6 +8,7 @@
 namespace Flame\Doctrine\Values;
 
 use Flame\Doctrine\IValidator;
+use Nette\DI\Container;
 use Nette\InvalidStateException;
 use Nette\Object;
 use Nette\Utils\Validators;
@@ -15,15 +16,15 @@ use Nette\Utils\Validators;
 abstract class DataSet extends Object implements IDataSet
 {
 
-	/** @var IValidator[]  */
-	private $validators = array();
+	/** @var  Container */
+	private $context;
 
 	/**
-	 * @param array $values
+	 * @param Container $context
 	 */
-	public function __construct($values = array())
+	function __construct(Container $context)
 	{
-		$this->setValues($values);
+		$this->context = $context;
 	}
 
 	/**
@@ -36,33 +37,33 @@ abstract class DataSet extends Object implements IDataSet
 		$properties = $this->getReflection()->getProperties();
 
 		foreach ($properties as $property) {
+			$property->setAccessible(true);
+
 			$isRequired = $property->getAnnotation('required');
 			if($isRequired && !isset($values[$property->name])) {
 				throw new InvalidStateException('Missing desired key "' . $property->name . '"');
 			}
 
-			if(isset($values[$property->name])) {
-				$property->setAccessible(true);
-				$property->setValue($this, $values[$property->name]);
+			if(!isset($values[$property->name])) {
+				continue;
 			}
+
+			$value = $values[$property->name];
+			if($class = $property->getAnnotation('validator')) {
+				$value = $this->getValidator($class)->validate($value);
+			}
+			
+			if($value === null) {
+				continue;
+			}
+
+			if($type = $property->getAnnotation('var')) {
+				Validators::assert($value, $type);
+			}
+
+			$property->setValue($this, $value);
 		}
 
-		return $this;
-	}
-
-	/**
-	 * @param IValidator $validator
-	 * @return $this
-	 * @throws \Nette\InvalidStateException
-	 */
-	public function addValidator(IValidator $validator)
-	{
-		$type = get_class($validator);
-		if(isset($this->validators[$type])) {
-			throw new InvalidStateException('Validator with same type "' . $type . '"  exist in the class "' . __CLASS__ . '" yet.');
-		}
-
-		$this->validators[$type] = $validator;
 		return $this;
 	}
 
@@ -72,28 +73,31 @@ abstract class DataSet extends Object implements IDataSet
 	 */
 	public function getValues()
 	{
-		$properties = $this->getReflection()->getProperties();
-
+		$vars = $this->getReflection()->getProperties();
 		$valid = array();
-		foreach ($properties as $property) {
-			$property->setAccessible(true);
-			$value = $property->getValue($this);
-
-			$validator = trim($property->getAnnotation('validator'), '\\');
-
-			if($validator && isset($this->validators[(string) $validator])) {
-				$value = $this->validators[$validator]->validate($value);
-			}
-
-			if ($value !== null) {
-				if($type = $property->getAnnotation('var')) {
-					Validators::assert($value, $type);
-				}
-
-				$valid[$property->name] = $value;
+		foreach ($vars as $var) {
+			$var->setAccessible(true);
+			$value = $var->getValue($this);
+			if($value !== null) {
+				$valid[$var->name] = $value;
 			}
 		}
 
 		return $valid;
+	}
+
+	/**
+	 * @param $class
+	 * @return IValidator
+	 * @throws \Nette\InvalidStateException
+	 */
+	private function getValidator($class)
+	{
+		$validator = $this->context->getByType($class);
+		if($validator instanceof IValidator) {
+			return $validator;
+		}
+
+		throw new InvalidStateException('Object "' . $class . '" is not instance of Flame\Doctrine\IValidator');
 	}
 }
--- Flame/Doctrine/Values/IDataSet.php
+++ Flame/Doctrine/Values/IDataSet.php
@@ -7,17 +7,8 @@
  */
 namespace Flame\Doctrine\Values;
 
-use Flame\Doctrine\IValidator;
-
 interface IDataSet
 {
-
-	/**
-	 * @param string $name Property name
-	 * @param IValidator $validator
-	 * @return $this
-	 */
-	public function addValidator(IValidator $validator);
 
 	/**
 	 * @param $values
