67be5b24fac5cfa593ab98422e1b5568aa31dfa7
JSifalda <sifalda.jiri@gmail.com>
2013-09-15 12:39:16 +0200

    Create CrudFactory

---
+++ Flame/Doctrine/Crud/CrudFactory.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * Class CrudFactory
+ *
+ * @author: Jiří Šifalda <sifalda.jiri@gmail.com>
+ * @date: 15.09.13
+ */
+namespace Flame\Doctrine\Crud;
+
+use Flame\Doctrine\Crud\Create\EntityCreator;
+use Flame\Doctrine\Crud\Delete\EntityDeleter;
+use Flame\Doctrine\Crud\Update\EntityUpdater;
+use Flame\Doctrine\EntityDao;
+
+abstract class CrudFactory
+{
+
+	/** @var \Flame\Doctrine\EntityDao  */
+	private $dao;
+
+	/**
+	 * @param EntityDao $dao
+	 */
+	function __construct(EntityDao $dao)
+	{
+		$this->dao = $dao;
+	}
+
+	/**
+	 * @return EntityCreator
+	 */
+	public function createCreator()
+	{
+		return new EntityCreator($this->dao);
+	}
+
+	/**
+	 * @return EntityUpdater
+	 */
+	public function createUpdater()
+	{
+		return new EntityUpdater($this->dao);
+	}
+
+	/**
+	 * @return EntityDeleter
+	 */
+	public function createDeleter()
+	{
+		return new EntityDeleter($this->dao);
+	}
+}
